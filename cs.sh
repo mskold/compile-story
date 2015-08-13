2362251dc0f0784894572611ede1091e1c1ccf11
mskold <markus.skold@kb.se>
2015-08-13 14:52:30 +0200

    Set utf-8 encoding for python

--- cs.sh
+++ cs.sh
@@ -5,9 +5,11 @@ OUTPUT_PATH="/tmp/cs/output"
 DRAFT_PATH="/tmp/cs/draft"
 
 export PATH="${PATH}:."
+export PYTHONIOENCODING="UTF-8"
 
 rm -fr "$DRAFT_PATH"
 rm -fr "$OUTPUT_PATH"
+
 python downdraft.py "$1"
 
 compile_story.sh $OUTPUT_PATH $DRAFT_PATH
