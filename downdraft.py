af506441204e5131f62f5cc9cd6efe74c82188dd
mskold <markus.skold@kb.se>
2015-08-14 09:24:47 +0200

    use share to simplify downloading folders

--- downdraft.py
+++ downdraft.py
@@ -1,16 +1,16 @@
 # Include the Dropbox SDK
 import dropbox
+import urllib2, zipfile
 import os, sys, errno
 from os.path import join, isdir, basename
 
 from config import download_access_token
 
-TMP_DRAFT_DIRECTORY='/tmp/cs/draft'
-NOVEL_DRAFT_DIR='/_text/'
-SHORT_STORY_DRAFT_DIR='/_text/_noveller'
+TMP_DRAFT_DIRECTORY='/tmp/cs/draft/'
+TEXT_DIR='/_text/'
 
 if len(sys.argv) < 2:
-    print "Usage: %s <storyname>" % sys.argv[0]
+    print("Usage: %s <storyname>" % sys.argv[0])
     exit(1)
 
 storyname = sys.argv[1]
@@ -25,18 +25,22 @@ def mkdir_p(path):
 
 client = dropbox.client.DropboxClient(download_access_token)
 try:
-    folder_metadata = client.metadata(join(NOVEL_DRAFT_DIR, storyname, 'draft'))
+    share_metadata = client.share(join(TEXT_DIR, storyname, 'draft'), short_url=False)
 except dropbox.rest.ErrorResponse as e:
-    print "%s apparently not a novel. Trying short story ..." % storyname
-    folder_metadata = client.metadata(join(SHORT_STORY_DRAFT_DIR, storyname, 'draft'))
+    print("%s apparently not a novel. Trying short story ..." % storyname)
+    share_metadata = client.share(join(TEXT_DIR, '_noveller', storyname, 'draft'), short_url=False)
 
 mkdir_p(TMP_DRAFT_DIRECTORY)
 
-for scene in folder_metadata['contents']:
-    if scene['is_dir'] == False:
-        filename = basename(scene['path'])
-        outfile = join(TMP_DRAFT_DIRECTORY, filename)
-        out = open(outfile, 'wb')
-        with client.get_file(scene['path']) as f:
-            print 'downloading', scene['path']
-            out.write(f.read())
+shareurl = share_metadata['url'].replace('?dl=0','?dl=1')
+print("Downloading %s" % shareurl)
+drafturl = urllib2.urlopen(shareurl)
+
+with open(join(TMP_DRAFT_DIRECTORY, 'draft.zip'), 'wb') as out:
+    out.write(drafturl.read())
+
+
+zfile = zipfile.ZipFile(join(TMP_DRAFT_DIRECTORY, 'draft.zip'))
+zfile.extractall(TMP_DRAFT_DIRECTORY)
+
+os.remove(join(TMP_DRAFT_DIRECTORY, 'draft.zip'))
